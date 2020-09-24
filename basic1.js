443affc839e115cdcb11cc12e7f11fa0306e4077
Christian00000 <c.froemmer@gmail.com>
2020-09-24 11:08:50 +0200

    add files

---
+++ basic1.js
@@ -0,0 +1,4 @@
+var sandwiches = { "sandwich": "hamburger", "calories": "260" };
+var fries = { "fries_size": "Large French Fries", "calories": "570" };
+
+document.getElementById("result").innerHTML = " My favorite sandwich is " + sandwiches.sandwich +" wich has approximately " + sandwiches.calories + " calories, along with it I enjoy eating, " + fries.fries_size + " which have about " + fries.calories + " calories."; 
